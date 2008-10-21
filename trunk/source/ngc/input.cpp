543d6d243721bd6798007e42d242553170ed4ce5
dborth <dborth@d154fe42-be53-0410-aac0-356034171556>
2008-10-21 16:43:18 +0000

    justifier fix

--- trunk/source/ngc/input.cpp
+++ trunk/source/ngc/input.cpp
@@ -512,11 +512,11 @@ void SetControllers ()
 	}
 	else if (Settings.JustifierMaster == true)
 	{
-		S9xSetController(0, CTL_JUSTIFIER, 0, 0, 0, 0);
+		S9xSetController (0, CTL_JOYPAD, 0, 0, 0, 0);
 		if(GCSettings.Justifier == 2)
 			S9xSetController(1, CTL_JUSTIFIER, 1, 0, 0, 0);
 		else
-			S9xSetController (1, CTL_JOYPAD, 1, 0, 0, 0);
+			S9xSetController(1, CTL_JUSTIFIER, 0, 0, 0, 0);
 	}
 	else
 	{
