fe3c660aa745635a740d7697cb5ed5aaedf0e692
dborth <dborth@d154fe42-be53-0410-aac0-356034171556>
2009-07-23 03:28:38 +0000

    [4.0.9 - July 22, 2009]
    * Fixed "No game saves found." message when there are actually saves.
    * Fixed shift key on keyboard
    * Text scrolling works again
    * Change default prompt window selection to "Cancel" button

---
+++ tags/4.0.9/source/ngc/gui/gui_filebrowser.cpp
@@ -0,0 +1,409 @@
+/****************************************************************************
+ * libwiigui
+ *
+ * Tantric 2009
+ *
+ * gui_filebrowser.cpp
+ *
+ * GUI class definitions
+ ***************************************************************************/
+
+#include "gui.h"
+#include "filebrowser.h"
+
+/**
+ * Constructor for the GuiFileBrowser class.
+ */
+GuiFileBrowser::GuiFileBrowser(int w, int h)
+{
+	width = w;
+	height = h;
+	numEntries = 0;
+	selectedItem = 0;
+	selectable = true;
+	listChanged = true; // trigger an initial list update
+	focus = 0; // allow focus
+
+	trigA = new GuiTrigger;
+	if(GCSettings.WiimoteOrientation)
+		trigA->SetSimpleTrigger(-1, WPAD_BUTTON_2 | WPAD_CLASSIC_BUTTON_A, PAD_BUTTON_A);
+	else
+		trigA->SetSimpleTrigger(-1, WPAD_BUTTON_A | WPAD_CLASSIC_BUTTON_A, PAD_BUTTON_A);
+
+	trigHeldA = new GuiTrigger;
+	trigHeldA->SetHeldTrigger(-1, WPAD_BUTTON_A | WPAD_CLASSIC_BUTTON_A, PAD_BUTTON_A);
+
+	btnSoundOver = new GuiSound(button_over_pcm, button_over_pcm_size, SOUND_PCM);
+	btnSoundClick = new GuiSound(button_click_pcm, button_click_pcm_size, SOUND_PCM);
+
+	bgGameSelection = new GuiImageData(bg_game_selection_png);
+	bgGameSelectionImg = new GuiImage(bgGameSelection);
+	bgGameSelectionImg->SetParent(this);
+	bgGameSelectionImg->SetAlignment(ALIGN_LEFT, ALIGN_MIDDLE);
+
+	bgGameSelectionEntry = new GuiImageData(bg_game_selection_entry_png);
+	gameFolder = new GuiImageData(folder_png);
+
+	scrollbar = new GuiImageData(scrollbar_png);
+	scrollbarImg = new GuiImage(scrollbar);
+	scrollbarImg->SetParent(this);
+	scrollbarImg->SetAlignment(ALIGN_RIGHT, ALIGN_TOP);
+	scrollbarImg->SetPosition(0, 30);
+
+	arrowDown = new GuiImageData(scrollbar_arrowdown_png);
+	arrowDownImg = new GuiImage(arrowDown);
+	arrowDownOver = new GuiImageData(scrollbar_arrowdown_over_png);
+	arrowDownOverImg = new GuiImage(arrowDownOver);
+	arrowUp = new GuiImageData(scrollbar_arrowup_png);
+	arrowUpImg = new GuiImage(arrowUp);
+	arrowUpOver = new GuiImageData(scrollbar_arrowup_over_png);
+	arrowUpOverImg = new GuiImage(arrowUpOver);
+	scrollbarBox = new GuiImageData(scrollbar_box_png);
+	scrollbarBoxImg = new GuiImage(scrollbarBox);
+	scrollbarBoxOver = new GuiImageData(scrollbar_box_over_png);
+	scrollbarBoxOverImg = new GuiImage(scrollbarBoxOver);
+
+	arrowUpBtn = new GuiButton(arrowUpImg->GetWidth(), arrowUpImg->GetHeight());
+	arrowUpBtn->SetParent(this);
+	arrowUpBtn->SetImage(arrowUpImg);
+	arrowUpBtn->SetImageOver(arrowUpOverImg);
+	arrowUpBtn->SetAlignment(ALIGN_RIGHT, ALIGN_TOP);
+	arrowUpBtn->SetSelectable(false);
+	arrowUpBtn->SetClickable(false);
+	arrowUpBtn->SetHoldable(true);
+	arrowUpBtn->SetTrigger(trigHeldA);
+	arrowUpBtn->SetSoundOver(btnSoundOver);
+	arrowUpBtn->SetSoundClick(btnSoundClick);
+
+	arrowDownBtn = new GuiButton(arrowDownImg->GetWidth(), arrowDownImg->GetHeight());
+	arrowDownBtn->SetParent(this);
+	arrowDownBtn->SetImage(arrowDownImg);
+	arrowDownBtn->SetImageOver(arrowDownOverImg);
+	arrowDownBtn->SetAlignment(ALIGN_RIGHT, ALIGN_BOTTOM);
+	arrowDownBtn->SetSelectable(false);
+	arrowDownBtn->SetClickable(false);
+	arrowDownBtn->SetHoldable(true);
+	arrowDownBtn->SetTrigger(trigHeldA);
+	arrowDownBtn->SetSoundOver(btnSoundOver);
+	arrowDownBtn->SetSoundClick(btnSoundClick);
+
+	scrollbarBoxBtn = new GuiButton(scrollbarBoxImg->GetWidth(), scrollbarBoxImg->GetHeight());
+	scrollbarBoxBtn->SetParent(this);
+	scrollbarBoxBtn->SetImage(scrollbarBoxImg);
+	scrollbarBoxBtn->SetImageOver(scrollbarBoxOverImg);
+	scrollbarBoxBtn->SetAlignment(ALIGN_RIGHT, ALIGN_TOP);
+	scrollbarBoxBtn->SetMinY(0);
+	scrollbarBoxBtn->SetMaxY(156);
+	scrollbarBoxBtn->SetSelectable(false);
+	scrollbarBoxBtn->SetClickable(false);
+	scrollbarBoxBtn->SetHoldable(true);
+	scrollbarBoxBtn->SetTrigger(trigHeldA);
+
+	for(int i=0; i<FILE_PAGESIZE; i++)
+	{
+		fileListText[i] = new GuiText(NULL, 20, (GXColor){0, 0, 0, 0xff});
+		fileListText[i]->SetAlignment(ALIGN_LEFT, ALIGN_MIDDLE);
+		fileListText[i]->SetPosition(5,0);
+		fileListText[i]->SetMaxWidth(380);
+
+		fileListBg[i] = new GuiImage(bgGameSelectionEntry);
+		fileListFolder[i] = new GuiImage(gameFolder);
+
+		fileList[i] = new GuiButton(380, 26);
+		fileList[i]->SetParent(this);
+		fileList[i]->SetLabel(fileListText[i]);
+		fileList[i]->SetImageOver(fileListBg[i]);
+		fileList[i]->SetPosition(2,26*i+3);
+		fileList[i]->SetTrigger(trigA);
+		fileList[i]->SetSoundClick(btnSoundClick);
+	}
+}
+
+/**
+ * Destructor for the GuiFileBrowser class.
+ */
+GuiFileBrowser::~GuiFileBrowser()
+{
+	delete arrowUpBtn;
+	delete arrowDownBtn;
+	delete scrollbarBoxBtn;
+
+	delete bgGameSelectionImg;
+	delete scrollbarImg;
+	delete arrowDownImg;
+	delete arrowDownOverImg;
+	delete arrowUpImg;
+	delete arrowUpOverImg;
+	delete scrollbarBoxImg;
+	delete scrollbarBoxOverImg;
+
+	delete bgGameSelection;
+	delete bgGameSelectionEntry;
+	delete gameFolder;
+	delete scrollbar;
+	delete arrowDown;
+	delete arrowDownOver;
+	delete arrowUp;
+	delete arrowUpOver;
+	delete scrollbarBox;
+	delete scrollbarBoxOver;
+
+	delete btnSoundOver;
+	delete btnSoundClick;
+	delete trigHeldA;
+	delete trigA;
+
+	for(int i=0; i<FILE_PAGESIZE; i++)
+	{
+		delete fileListText[i];
+		delete fileList[i];
+		delete fileListBg[i];
+		delete fileListFolder[i];
+	}
+}
+
+void GuiFileBrowser::SetFocus(int f)
+{
+	focus = f;
+
+	for(int i=0; i<FILE_PAGESIZE; i++)
+		fileList[i]->ResetState();
+
+	if(f == 1)
+		fileList[selectedItem]->SetState(STATE_SELECTED);
+}
+
+void GuiFileBrowser::ResetState()
+{
+	state = STATE_DEFAULT;
+	stateChan = -1;
+	selectedItem = 0;
+
+	for(int i=0; i<FILE_PAGESIZE; i++)
+	{
+		fileList[i]->ResetState();
+	}
+}
+
+void GuiFileBrowser::TriggerUpdate()
+{
+	listChanged = true;
+}
+
+/**
+ * Draw the button on screen
+ */
+void GuiFileBrowser::Draw()
+{
+	if(!this->IsVisible())
+		return;
+
+	bgGameSelectionImg->Draw();
+
+	for(int i=0; i<FILE_PAGESIZE; i++)
+	{
+		fileList[i]->Draw();
+	}
+
+	scrollbarImg->Draw();
+	arrowUpBtn->Draw();
+	arrowDownBtn->Draw();
+	scrollbarBoxBtn->Draw();
+
+	this->UpdateEffects();
+}
+
+void GuiFileBrowser::Update(GuiTrigger * t)
+{
+	if(state == STATE_DISABLED || !t)
+		return;
+
+	int position = 0;
+	int positionWiimote = 0;
+
+	arrowUpBtn->Update(t);
+	arrowDownBtn->Update(t);
+	scrollbarBoxBtn->Update(t);
+
+	// move the file listing to respond to wiimote cursor movement
+	if(scrollbarBoxBtn->GetState() == STATE_HELD &&
+		scrollbarBoxBtn->GetStateChan() == t->chan &&
+		t->wpad.ir.valid &&
+		browser.numEntries > FILE_PAGESIZE
+		)
+	{
+		scrollbarBoxBtn->SetPosition(0,0);
+		positionWiimote = t->wpad.ir.y - 60 - scrollbarBoxBtn->GetTop();
+
+		if(positionWiimote < scrollbarBoxBtn->GetMinY())
+			positionWiimote = scrollbarBoxBtn->GetMinY();
+		else if(positionWiimote > scrollbarBoxBtn->GetMaxY())
+			positionWiimote = scrollbarBoxBtn->GetMaxY();
+
+		browser.pageIndex = (positionWiimote * browser.numEntries)/156.0 - selectedItem;
+
+		if(browser.pageIndex <= 0)
+		{
+			browser.pageIndex = 0;
+		}
+		else if(browser.pageIndex+FILE_PAGESIZE >= browser.numEntries)
+		{
+			browser.pageIndex = browser.numEntries-FILE_PAGESIZE;
+		}
+		listChanged = true;
+		focus = false;
+	}
+
+	if(arrowDownBtn->GetState() == STATE_HELD && arrowDownBtn->GetStateChan() == t->chan)
+	{
+		t->wpad.btns_h |= WPAD_BUTTON_DOWN;
+		if(!this->IsFocused())
+			((GuiWindow *)this->GetParent())->ChangeFocus(this);
+	}
+	else if(arrowUpBtn->GetState() == STATE_HELD && arrowUpBtn->GetStateChan() == t->chan)
+	{
+		t->wpad.btns_h |= WPAD_BUTTON_UP;
+		if(!this->IsFocused())
+			((GuiWindow *)this->GetParent())->ChangeFocus(this);
+	}
+
+	// pad/joystick navigation
+	if(!focus)
+	{
+		goto endNavigation; // skip navigation
+		listChanged = false;
+	}
+
+	if(t->Right())
+	{
+		if(browser.pageIndex < browser.numEntries && browser.numEntries > FILE_PAGESIZE)
+		{
+			browser.pageIndex += FILE_PAGESIZE;
+			if(browser.pageIndex+FILE_PAGESIZE >= browser.numEntries)
+				browser.pageIndex = browser.numEntries-FILE_PAGESIZE;
+			listChanged = true;
+		}
+	}
+	else if(t->Left())
+	{
+		if(browser.pageIndex > 0)
+		{
+			browser.pageIndex -= FILE_PAGESIZE;
+			if(browser.pageIndex < 0)
+				browser.pageIndex = 0;
+			listChanged = true;
+		}
+	}
+	else if(t->Down())
+	{
+		if(browser.pageIndex + selectedItem + 1 < browser.numEntries)
+		{
+			if(selectedItem == FILE_PAGESIZE-1)
+			{
+				// move list down by 1
+				browser.pageIndex++;
+				listChanged = true;
+			}
+			else if(fileList[selectedItem+1]->IsVisible())
+			{
+				fileList[selectedItem]->ResetState();
+				fileList[++selectedItem]->SetState(STATE_SELECTED, t->chan);
+			}
+		}
+	}
+	else if(t->Up())
+	{
+		if(selectedItem == 0 &&	browser.pageIndex + selectedItem > 0)
+		{
+			// move list up by 1
+			browser.pageIndex--;
+			listChanged = true;
+		}
+		else if(selectedItem > 0)
+		{
+			fileList[selectedItem]->ResetState();
+			fileList[--selectedItem]->SetState(STATE_SELECTED, t->chan);
+		}
+	}
+
+	endNavigation:
+
+	for(int i=0; i<FILE_PAGESIZE; i++)
+	{
+		if(listChanged || numEntries != browser.numEntries)
+		{
+			if(browser.pageIndex+i < browser.numEntries)
+			{
+				if(fileList[i]->GetState() == STATE_DISABLED)
+					fileList[i]->SetState(STATE_DEFAULT);
+
+				fileList[i]->SetVisible(true);
+
+				fileListText[i]->SetText(browserList[browser.pageIndex+i].displayname);
+
+				if(browserList[browser.pageIndex+i].isdir) // directory
+				{
+					fileList[i]->SetIcon(fileListFolder[i]);
+					fileListText[i]->SetPosition(30,0);
+				}
+				else
+				{
+					fileList[i]->SetIcon(NULL);
+					fileListText[i]->SetPosition(10,0);
+				}
+			}
+			else
+			{
+				fileList[i]->SetVisible(false);
+				fileList[i]->SetState(STATE_DISABLED);
+			}
+		}
+
+		if(i != selectedItem && fileList[i]->GetState() == STATE_SELECTED)
+			fileList[i]->ResetState();
+		else if(focus && i == selectedItem && fileList[i]->GetState() == STATE_DEFAULT)
+			fileList[selectedItem]->SetState(STATE_SELECTED, t->chan);
+
+		int currChan = t->chan;
+
+		if(t->wpad.ir.valid && !fileList[i]->IsInside(t->wpad.ir.x, t->wpad.ir.y))
+			t->chan = -1;
+
+		fileList[i]->Update(t);
+		t->chan = currChan;
+
+		if(fileList[i]->GetState() == STATE_SELECTED)
+		{
+			selectedItem = i;
+			browser.selIndex = browser.pageIndex + i;
+		}
+
+		if(selectedItem == i)
+			fileListText[i]->SetScroll(SCROLL_HORIZONTAL);
+		else
+			fileListText[i]->SetScroll(SCROLL_NONE);
+	}
+
+	// update the location of the scroll box based on the position in the file list
+	if(positionWiimote > 0)
+	{
+		position = positionWiimote; // follow wiimote cursor
+	}
+	else
+	{
+		position = 156*(browser.pageIndex + FILE_PAGESIZE/2.0) / (browser.numEntries*1.0);
+
+		if(browser.pageIndex/(FILE_PAGESIZE/2.0) < 1)
+			position = 0;
+		else if((browser.pageIndex+FILE_PAGESIZE)/(FILE_PAGESIZE*1.0) >= (browser.numEntries)/(FILE_PAGESIZE*1.0))
+			position = 156;
+	}
+
+	scrollbarBoxBtn->SetPosition(0,position+36);
+
+	listChanged = false;
+	numEntries = browser.numEntries;
+
+	if(updateCB)
+		updateCB(this);
+}
---
+++ tags/4.0.9/source/ngc/gui/gui_text.cpp
@@ -0,0 +1,348 @@
+/****************************************************************************
+ * libwiigui
+ *
+ * Tantric 2009
+ *
+ * gui_text.cpp
+ *
+ * GUI class definitions
+ ***************************************************************************/
+
+#include "gui.h"
+
+static int currentSize = 0;
+static int presetSize = 0;
+static int presetMaxWidth = 0;
+static int presetAlignmentHor = 0;
+static int presetAlignmentVert = 0;
+static u16 presetStyle = 0;
+static GXColor presetColor = (GXColor){255, 255, 255, 255};
+
+#define TEXT_SCROLL_DELAY			8
+#define	TEXT_SCROLL_INITIAL_DELAY	6
+
+/**
+ * Constructor for the GuiText class.
+ */
+GuiText::GuiText(const char * t, int s, GXColor c)
+{
+	origText = NULL;
+	text = NULL;
+	size = s;
+	color = c;
+	alpha = c.a;
+	style = FTGX_JUSTIFY_CENTER | FTGX_ALIGN_MIDDLE;
+	maxWidth = 0;
+	wrap = false;
+	textDyn = NULL;
+	textScroll = SCROLL_NONE;
+	textScrollPos = 0;
+	textScrollInitialDelay = TEXT_SCROLL_INITIAL_DELAY;
+	textScrollDelay = TEXT_SCROLL_DELAY;
+
+	alignmentHor = ALIGN_CENTRE;
+	alignmentVert = ALIGN_MIDDLE;
+
+	if(t)
+	{
+		origText = strdup(t);
+		text = charToWideChar(t);
+	}
+}
+
+/**
+ * Constructor for the GuiText class, uses presets
+ */
+GuiText::GuiText(const char * t)
+{
+	origText = NULL;
+	text = NULL;
+	size = presetSize;
+	color = presetColor;
+	alpha = presetColor.a;
+	style = presetStyle;
+	maxWidth = presetMaxWidth;
+	wrap = false;
+	textDyn = NULL;
+	textScroll = SCROLL_NONE;
+	textScrollPos = 0;
+	textScrollInitialDelay = TEXT_SCROLL_INITIAL_DELAY;
+	textScrollDelay = TEXT_SCROLL_DELAY;
+
+	alignmentHor = presetAlignmentHor;
+	alignmentVert = presetAlignmentVert;
+
+	if(t)
+	{
+		origText = strdup(t);
+		text = charToWideChar(t);
+	}
+}
+
+/**
+ * Destructor for the GuiText class.
+ */
+GuiText::~GuiText()
+{
+	if(origText)
+		free(origText);
+	if(text)
+		delete text;
+	if(textDyn)
+		delete textDyn;
+}
+
+void GuiText::SetText(const char * t)
+{
+	if(origText)
+		free(origText);
+	if(text)
+		delete text;
+	if(textDyn)
+		delete textDyn;
+
+	origText = NULL;
+	text = NULL;
+	textDyn = NULL;
+	textScrollPos = 0;
+	textScrollInitialDelay = TEXT_SCROLL_INITIAL_DELAY;
+
+	if(t)
+	{
+		origText = strdup(t);
+		text = charToWideChar(t);
+	}
+}
+
+void GuiText::SetPresets(int sz, GXColor c, int w, u16 s, int h, int v)
+{
+	presetSize = sz;
+	presetColor = c;
+	presetStyle = s;
+	presetMaxWidth = w;
+	presetAlignmentHor = h;
+	presetAlignmentVert = v;
+}
+
+void GuiText::SetFontSize(int s)
+{
+	size = s;
+}
+
+void GuiText::SetMaxWidth(int width)
+{
+	maxWidth = width;
+}
+
+void GuiText::SetWrap(bool w, int width)
+{
+	wrap = w;
+	maxWidth = width;
+}
+
+void GuiText::SetScroll(int s)
+{
+	if(textScroll == s)
+		return;
+
+	if(textDyn)
+	{
+		delete(textDyn);
+		textDyn = NULL;
+	}
+	textScroll = s;
+	textScrollPos = 0;
+	textScrollInitialDelay = TEXT_SCROLL_INITIAL_DELAY;
+	textScrollDelay = TEXT_SCROLL_DELAY;
+}
+
+void GuiText::SetColor(GXColor c)
+{
+	color = c;
+	alpha = c.a;
+}
+
+void GuiText::SetStyle(u16 s)
+{
+	style = s;
+}
+
+void GuiText::SetAlignment(int hor, int vert)
+{
+	style = 0;
+
+	switch(hor)
+	{
+		case ALIGN_LEFT:
+			style |= FTGX_JUSTIFY_LEFT;
+			break;
+		case ALIGN_RIGHT:
+			style |= FTGX_JUSTIFY_RIGHT;
+			break;
+		default:
+			style |= FTGX_JUSTIFY_CENTER;
+			break;
+	}
+	switch(vert)
+	{
+		case ALIGN_TOP:
+			style |= FTGX_ALIGN_TOP;
+			break;
+		case ALIGN_BOTTOM:
+			style |= FTGX_ALIGN_BOTTOM;
+			break;
+		default:
+			style |= FTGX_ALIGN_MIDDLE;
+			break;
+	}
+
+	alignmentHor = hor;
+	alignmentVert = vert;
+}
+
+/**
+ * Draw the text on screen
+ */
+void GuiText::Draw()
+{
+	if(!text)
+		return;
+
+	if(!this->IsVisible())
+		return;
+
+	GXColor c = color;
+	c.a = this->GetAlpha();
+
+	int newSize = size*this->GetScale();
+
+	if(newSize != currentSize)
+	{
+		ChangeFontSize(newSize);
+		if(!fontSystem[newSize])
+			fontSystem[newSize] = new FreeTypeGX(newSize);
+		currentSize = newSize;
+	}
+
+	int voffset = 0;
+
+	if(alignmentVert == ALIGN_MIDDLE)
+		voffset = -newSize/2 + 2;
+
+	if(maxWidth > 0)
+	{
+		char * tmpText = strdup(origText);
+		u8 maxChar = (maxWidth*2.0) / newSize;
+
+		if(!textDyn)
+		{
+			if(strlen(tmpText) > maxChar)
+				tmpText[maxChar] = 0;
+			textDyn = charToWideChar(tmpText);
+		}
+
+		if(textScroll == SCROLL_HORIZONTAL)
+		{
+			int textlen = strlen(origText);
+
+			if(textlen > maxChar && (FrameTimer % textScrollDelay == 0))
+			{
+				if(textScrollInitialDelay)
+				{
+					textScrollInitialDelay--;
+				}
+				else
+				{
+					textScrollPos++;
+					if(textScrollPos > textlen-1)
+					{
+						textScrollPos = 0;
+						textScrollInitialDelay = TEXT_SCROLL_INITIAL_DELAY;
+					}
+
+					strncpy(tmpText, &origText[textScrollPos], maxChar-1);
+					tmpText[maxChar-1] = 0;
+
+					int dynlen = strlen(tmpText);
+
+					if(dynlen+2 < maxChar)
+					{
+						tmpText[dynlen] = ' ';
+						tmpText[dynlen+1] = ' ';
+						strncat(&tmpText[dynlen+2], origText, maxChar - dynlen - 2);
+					}
+					if(textDyn) delete textDyn;
+					textDyn = charToWideChar(tmpText);
+				}
+			}
+			if(textDyn)
+				fontSystem[currentSize]->drawText(this->GetLeft(), this->GetTop()+voffset, textDyn, c, style);
+		}
+		else if(wrap)
+		{
+			int lineheight = newSize + 6;
+			int txtlen = wcslen(text);
+			int i = 0;
+			int ch = 0;
+			int linenum = 0;
+			int lastSpace = -1;
+			int lastSpaceIndex = -1;
+			wchar_t * textrow[20];
+
+			while(ch < txtlen)
+			{
+				if(i == 0)
+					textrow[linenum] = new wchar_t[txtlen + 1];
+
+				textrow[linenum][i] = text[ch];
+				textrow[linenum][i+1] = 0;
+
+				if(text[ch] == ' ' || ch == txtlen-1)
+				{
+					if(wcslen(textrow[linenum]) >= maxChar)
+					{
+						if(lastSpace >= 0)
+						{
+							textrow[linenum][lastSpaceIndex] = 0; // discard space, and everything after
+							ch = lastSpace; // go backwards to the last space
+							lastSpace = -1; // we have used this space
+							lastSpaceIndex = -1;
+						}
+						linenum++;
+						i = -1;
+					}
+					else if(ch == txtlen-1)
+					{
+						linenum++;
+					}
+				}
+				if(text[ch] == ' ' && i >= 0)
+				{
+					lastSpace = ch;
+					lastSpaceIndex = i;
+				}
+				ch++;
+				i++;
+			}
+
+			if(alignmentVert == ALIGN_MIDDLE)
+				voffset = voffset - (lineheight*linenum)/2 + lineheight/2;
+
+			for(i=0; i < linenum; i++)
+			{
+				fontSystem[currentSize]->drawText(this->GetLeft(), this->GetTop()+voffset+i*lineheight, textrow[i], c, style);
+				delete textrow[i];
+			}
+		}
+		else
+		{
+			fontSystem[currentSize]->drawText(this->GetLeft(), this->GetTop()+voffset, textDyn, c, style);
+		}
+		free(tmpText);
+	}
+	else
+	{
+		fontSystem[currentSize]->drawText(this->GetLeft(), this->GetTop()+voffset, text, c, style);
+	}
+	this->UpdateEffects();
+}
