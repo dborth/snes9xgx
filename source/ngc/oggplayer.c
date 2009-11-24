35ef5bd7e9414cf61ba88fabb96b9dc4884d4c63
dborth <dborth@d154fe42-be53-0410-aac0-356034171556>
2009-11-24 03:57:27 +0000

    update comments

--- source/ngc/oggplayer.c
+++ source/ngc/oggplayer.c
@@ -1,38 +1,41 @@
 /*
  Copyright (c) 2008 Francisco Muñoz 'Hermes' <www.elotrolado.net>
  All rights reserved.
-
+ 
  Proper (standard) vorbis usage by Tantric, 2009
  Threading modifications/corrections by Tantric, 2009
 
- Redistribution and use in source and binary forms, with or without modification, are
- permitted provided that the following conditions are met:
+ Redistribution and use in source and binary forms, with or without 
+ modification, are permitted provided that the following conditions are met:
 
- - Redistributions of source code must retain the above copyright notice, this list of
- conditions and the following disclaimer.
- - Redistributions in binary form must reproduce the above copyright notice, this list
- of conditions and the following disclaimer in the documentation and/or other
- materials provided with the distribution.
- - The names of the contributors may not be used to endorse or promote products derived
- from this software without specific prior written permission.
+ - Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright notice, 
+ this list of conditions and the following disclaimer in the documentation 
+ and/or other materials provided with the distribution.
+ - The names of the contributors may not be used to endorse or promote products 
+ derived from this software without specific prior written permission.
 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
- THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE 
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef NO_SOUND
-
-#include "oggplayer.h"
+#include <asndlib.h>
+#include <tremor/ivorbiscodec.h>
+#include <tremor/ivorbisfile.h>
 #include <gccore.h>
 #include <unistd.h>
 #include <string.h>
+
+#include "oggplayer.h"
 
 /* functions to read the Ogg file from memory */
 
@@ -440,11 +443,11 @@ void StopOgg()
 	}
 }
 
-int PlayOgg(char * buf, int buflen, int time_pos, int mode)
+int PlayOgg(const void *buffer, s32 len, int time_pos, int mode)
 {
 	StopOgg();
 
-	private_ogg.fd = mem_open(buf, buflen);
+	private_ogg.fd = mem_open((char *)buffer, len);
 	
 	if (private_ogg.fd < 0)
 	{
@@ -522,10 +525,8 @@ s32 GetTimeOgg()
 {
 	int ret;
 	if (ogg_thread_running == 0 || private_ogg.fd < 0)
-		return 0;
+		return -1;
 	ret = ((s32) ov_time_tell(&private_ogg.vf));
-	if (ret < 0)
-		ret = 0;
 
 	return ret;
 }
@@ -535,5 +536,3 @@ void SetTimeOgg(s32 time_pos)
 	if (time_pos >= 0)
 		private_ogg.seek_time = time_pos;
 }
-
-#endif
