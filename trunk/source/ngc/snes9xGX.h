60e4f210f752e250eea2e292b9d9a97fd416c66b
dborth <dborth@d154fe42-be53-0410-aac0-356034171556>
2009-12-02 07:39:05 +0000

    finalize 4.1.4

--- trunk/source/ngc/snes9xGX.h
+++ trunk/source/ngc/snes9xGX.h
@@ -20,7 +20,7 @@
 #include "filelist.h"
 
 #define APPNAME 		"Snes9x GX"
-#define APPVERSION 		"4.1.3"
+#define APPVERSION 		"4.1.4"
 #define APPFOLDER 		"snes9x"
 #define PREF_FILE_NAME 	"settings.xml"
 
