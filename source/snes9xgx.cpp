3aa626f2b527207d14cab7d091d881b91eed9f64
dborth <dborth@d154fe42-be53-0410-aac0-356034171556>
2010-07-20 06:45:04 +0000

    finalize 4.2.2

--- source/snes9xgx.cpp
+++ source/snes9xgx.cpp
@@ -252,31 +252,6 @@ static bool FindIOS(u32 ios)
     free(titles); 
 	return false;
 }
-
-#define ROUNDDOWN32(v)				(((u32)(v)-0x1f)&~0x1f)
-
-static bool USBLANDetected()
-{
-	u8 dummy;
-	u8 i;
-	u16 vid, pid;
-
-	USB_Initialize();
-	u8 *buffer = (u8*)ROUNDDOWN32(((u32)SYS_GetArena2Hi() - (32*1024)));
-	memset(buffer, 0, 8 << 3);
-
-	if(USB_GetDeviceList("/dev/usb/oh0", buffer, 8, 0, &dummy) < 0)
-		return false;
-
-	for(i = 0; i < 8; i++)
-	{
-		memcpy(&vid, (buffer + (i << 3) + 4), 2);
-		memcpy(&pid, (buffer + (i << 3) + 6), 2);
-		if( (vid == 0x0b95) && (pid == 0x7720))
-			return true;
-	}
-	return false;
-}
 #endif
 /****************************************************************************
  * USB Gecko Debugging
@@ -343,8 +318,6 @@ main(int argc, char *argv[])
 	#endif
 
 	#ifdef HW_RVL
-	bool usblan = USBLANDetected();
-
 	// try to load IOS 202
 	if(FindIOS(202))
 		IOS_ReloadIOS(202);
@@ -354,33 +327,11 @@ main(int argc, char *argv[])
 	if(IOS_GetVersion() == 202)
 	{
 		// enable DVD and USB2
+		DI_LoadDVDX(false);
+		DI_Init();
+
 		if(mload_init() >= 0 && load_ehci_module())
-		{
-			int mode = 3;
-
-			if(usblan)
-			{
-				int usblanport = GetUSB2LanPort();
-
-				if(usblanport >= 0)
-				{
-					if(usblanport == 1)
-						mode = 1;
-					else
-						mode = 2;
-
-					USB2Storage_Close();
-					mload_close();
-					IOS_ReloadIOS(202);
-					mload_init();
-					load_ehci_module();
-				}
-			}
-			SetUSB2Mode(mode);
 			USB2Enable(true);
-			DI_LoadDVDX(false);
-			DI_Init();
-		}
 	}
 	#endif
 
