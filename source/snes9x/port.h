796df8935db48bbe99fb1d31dcf538bd1eeb119d
dborth <dborth@d154fe42-be53-0410-aac0-356034171556>
2010-10-03 20:13:53 +0000

    sync with upstream

--- source/snes9x/port.h
+++ source/snes9x/port.h
@@ -181,8 +181,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-//#include <memory.h>
-#include <string.h>
+#ifndef GEKKO
+#include <memory.h>
+#endif
 #include <time.h>
 #include <string.h>
 #ifdef HAVE_STRINGS_H
@@ -225,11 +226,15 @@ typedef uint32_t			uint32;
 typedef int64_t				int64;
 typedef uint64_t			uint64;
 #else	// HAVE_STDINT_H
+#ifdef __WIN32__
+typedef intptr_t			pint;
+#else	// __WIN32__
 #ifdef PTR_NOT_INT
 typedef long				pint;
 #else
 typedef int					pint;
 #endif
+#endif	// __WIN32__
 #ifdef __WIN32__
 #ifdef __BORLANDC__
 #include <systypes.h>
@@ -301,6 +306,8 @@ void _makepath (char *, const char *, const char *, const char *, const char *);
 #define strncasecmp	strnicmp
 void WinDisplayStringFromBottom(const char *string, int linesFromBottom, int pixelsFromLeft, bool allowWrap);
 #define S9xDisplayString	WinDisplayStringFromBottom
+void SetInfoDlgColor(unsigned char, unsigned char, unsigned char);
+#define SET_UI_COLOR(r,g,b) SetInfoDlgColor(r,g,b)
 #endif
 
 #ifdef __DJGPP
@@ -324,7 +331,7 @@ void WinDisplayStringFromBottom(const char *string, int linesFromBottom, int pix
 #define TITLE "Snes9x"
 #endif
 
-#if defined(__i386__) || defined(__i486__) || defined(__i586__) || defined(__i686__) || defined(__x86_64__) || defined(__alpha__) || defined(__MIPSEL__) || defined(_M_IX86)
+#if defined(__i386__) || defined(__i486__) || defined(__i586__) || defined(__i686__) || defined(__x86_64__) || defined(__alpha__) || defined(__MIPSEL__) || defined(_M_IX86) || defined(_M_X64)
 #define LSB_FIRST
 #define FAST_LSB_WORD_ACCESS
 #else
