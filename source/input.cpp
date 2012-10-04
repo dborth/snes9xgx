fa7eaf3fedb8ce34b7a3f5d89a80344a0e43afe5
dborth <dborth@d154fe42-be53-0410-aac0-356034171556>
2012-10-04 00:33:54 +0000

    Fix lag with GameCube controllers

--- source/input.cpp
+++ source/input.cpp
@@ -197,8 +197,6 @@ void ResetControls(int consoleCtrl, int wiiCtrl)
  *
  * Scans pad and wpad
  ***************************************************************************/
-static int padsConnected = 0;
-static u64 prev, now;
 
 void
 UpdatePads()
@@ -207,17 +205,7 @@ UpdatePads()
 	WPAD_ScanPads();
 	#endif
 
-	now = gettime();
-
-	if(!padsConnected && diff_sec(prev, now) < 2)
-		return;
-
-	prev = now;
-
-	padsConnected = PAD_ScanPads();
-
-	if(!padsConnected)
-		return;
+	PAD_ScanPads();
 
 	for(int i=3; i >= 0; i--)
 	{
