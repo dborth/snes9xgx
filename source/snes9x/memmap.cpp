33b3e8c08b901e69b35415d2c0c3eb0b04382474
bladeoner <bladeoner112@gmail.com>
2019-03-04 10:55:34 +0100

    Fix blackscreen Rendering Rangers R2 (J)

--- source/snes9x/memmap.cpp
+++ source/snes9x/memmap.cpp
@@ -3646,8 +3646,8 @@ void CMemory::ApplyROMFixes (void)
 
 	if (!Settings.DisableGameSpecificHacks)
 	{
-		//if (match_id("AVCJ"))                                      // Rendering Ranger R2
-		//	Timings.APUSpeedup = 2;
+		if (match_id("AVCJ"))                                      // Rendering Ranger R2
+			Timings.APUSpeedup = 4;
 		if (match_id("AANJ"))                                      // Chou Aniki
 			Timings.APUSpeedup = -3;
 		if (match_na("CIRCUIT USA"))
