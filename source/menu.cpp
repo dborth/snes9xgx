14be4448232c226aeba1c6cca4bcfed9daa5c45e
Daryl Borth <dborth@gmail.com>
2026-04-13 14:10:41 -0600

    fix text positioning

--- source/menu.cpp
+++ source/menu.cpp
@@ -900,10 +900,10 @@ static void WindowCredits(void * ptr)
 
 	txt[i] = new GuiText(controllerInfo, 14, (GXColor){0, 0, 0, 255});
 	txt[i]->SetAlignment(ALIGN_LEFT, ALIGN_BOTTOM);
-	txt[i]->SetPosition(20,-60); i++;
+	txt[i]->SetPosition(20,-50); i++;
 	txt[i] = new GuiText(wiiDetails, 14, (GXColor){0, 0, 0, 255});
 	txt[i]->SetAlignment(ALIGN_RIGHT, ALIGN_BOTTOM);
-	txt[i]->SetPosition(-20,-60); i++;
+	txt[i]->SetPosition(-20,-70); i++;
 
 	GuiText::SetPresets(12, (GXColor){0, 0, 0, 255}, 0, FTGX_JUSTIFY_CENTER | FTGX_ALIGN_TOP, ALIGN_CENTRE, ALIGN_BOTTOM);
 
