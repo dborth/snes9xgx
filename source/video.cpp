e7ed2f87fcea3ce55949a185cb4b2dab2e1ed302
Daryl Borth <dborth@gmail.com>
2026-07-02 22:44:49 -0600

    video threading fixes

--- source/video.cpp
+++ source/video.cpp
@@ -280,6 +280,7 @@ static lwp_t vbthread = LWP_THREAD_NULL;
 static lwpq_t render_queue;          // Queue for the main thread to sleep on
 static lwpq_t vb_queue;              // Queue for the VSync thread to sleep on
 static volatile bool vb_done = true; // Tracks if the VSync thread has completed its wait
+static volatile bool vb_wait = false; // Tracks if the VSync thread should begin waiting
 
 /****************************************************************************
  * vbgetback
@@ -287,17 +288,26 @@ static volatile bool vb_done = true; // Tracks if the VSync thread has completed
  * This callback enables the emulator to keep running while waiting for a
  * vertical blank
  ***************************************************************************/
-static void *
-vbgetback (void *arg)
+static void * vbgetback (void *arg)
 {
 	while (1)
 	{
-		LWP_ThreadSleep(vb_queue);     // Sleep until kicked off by copy_to_xfb
-		VIDEO_WaitVSync ();	         /**< Wait for video vertical blank */
-		vb_done = true;
-		LWP_ThreadSignal(render_queue); // Instantly alert the main thread if it is waiting
-	}
+		u32 level;
+		_CPU_ISR_Disable(level);
+		while (!vb_wait)
+		{
+			LWP_ThreadSleep(vb_queue);     // Sleep safely until update_video kicks us off
+		}
+		vb_wait = false;
+		_CPU_ISR_Restore(level);
 
+		VIDEO_WaitVSync();                 // Wait for video vertical blank
+
+		_CPU_ISR_Disable(level);
+		vb_done = true;
+		LWP_ThreadSignal(render_queue);    // Instantly alert the main thread
+		_CPU_ISR_Restore(level);
+	}
 	return NULL;
 }
 
@@ -909,10 +919,14 @@ update_video (int width, int height)
 
 	// Wait for the VI to display the PREVIOUSLY submitted frame
 	// This naturally throttles the emulator to the TV's refresh rate
+	u32 level;
+
+	_CPU_ISR_Disable(level);
 	while (!vb_done || (copynow == GX_TRUE))
 	{
 		LWP_ThreadSleep(render_queue); // Halts main thread with 0 CPU load until signals occur
 	}
+	_CPU_ISR_Restore(level);
 
 	// Guarantee the GPU has fully finished rendering the previous frame
 	// before we begin swizzling new data into texturemem
@@ -1056,8 +1070,11 @@ update_video (int width, int height)
 	copynow = GX_TRUE;
 
 	// Reset state and signal background VSync thread to begin waiting for next blanking interval
+	_CPU_ISR_Disable(level);
 	vb_done = false;
+	vb_wait = true;
 	LWP_ThreadSignal(vb_queue);
+	_CPU_ISR_Restore(level);
 }
 
 void AllocGfxMem()
