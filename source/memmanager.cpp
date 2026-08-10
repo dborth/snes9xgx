ae13002b8425dba05ba5a7d16a4a8c30c2067894
Daryl Borth <dborth@gmail.com>
2026-08-10 17:32:45 -0600

    switch from lwp_heap to mspace

--- source/memmanager.cpp
+++ source/memmanager.cpp
@@ -8,7 +8,6 @@
  * Memory manager
  ***************************************************************************/
 
-#include <ogc/lwp_heap.h>
 #include <ogc/system.h>
 #include <malloc.h>
 #include "snes9xgx.h"
@@ -29,26 +28,37 @@ enum
 	MEMORY_MODE_GAME
 };
 
-static heap_cntrl extmem_heap;
+static mspace extmem_space = NULL;
 static int memoryMode = -1;
 u8 * romPtr = NULL;
 
 void InitMemManager ()
 {
+	void *base_ptr = NULL;
+	size_t capacity = 0;
+
 	#ifdef HW_DOL
 	VM_Init(ARAM_SIZE, MRAM_BACKING); // Setup Virtual Memory with the entire ARAM
-	__lwp_heap_init(&extmem_heap, (void *)ARAM_VM_BASE, ARAM_SIZE, 32);
+	base_ptr = (void *)ARAM_VM_BASE;
+	capacity = ARAM_SIZE;
+	#else
+	base_ptr = SYS_AllocArenaMem2Hi(MEM2_SIZE, 32);
+	capacity = MEM2_SIZE;
+	#endif
+
+	extmem_space = create_mspace_with_base(base_ptr, capacity, 0);
+	mspace_set_footprint_limit(extmem_space, capacity);
+
+	#ifdef HW_DOL
 	romPtr = (uint8 *)extmem_malloc(Memory.MAX_ROM_SIZE + 0x200 + 0x8000);
 	#else
-	void *mem2_heap_ptr = SYS_AllocArenaMem2Hi(MEM2_SIZE, 32);
-	__lwp_heap_init(&extmem_heap, mem2_heap_ptr, MEM2_SIZE, 32);
-	romPtr = (uint8 *) memalign(32,Memory.MAX_ROM_SIZE + 0x200 + 0x8000);
+	romPtr = (uint8 *) memalign(32, Memory.MAX_ROM_SIZE + 0x200 + 0x8000);
 	#endif
 }
 
 void* extmem_malloc(u32 size)
 {
-	return __lwp_heap_allocate(&extmem_heap, size);
+	return mspace_malloc(extmem_space, size);
 }
 
 char* extmem_strdup(const char *s)
@@ -67,14 +77,13 @@ char* extmem_strdup(const char *s)
 
 void extmem_free(void *ptr)
 {
-	__lwp_heap_free(&extmem_heap, ptr);
+	mspace_free(extmem_space, ptr);
 }
 
 int extmem_size_free()
 {
-	heap_iblock info;
-	__lwp_heap_getinfo(&extmem_heap,&info);
-	return info.free_size;
+	struct mallinfo info = mspace_mallinfo(extmem_space);
+	return info.fordblks;
 }
 
 void SwitchMemoryModeMenu() {
