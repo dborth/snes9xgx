38bf6c249f9cb6aeb7e6522ebfb73ece2153cd3e
Daryl Borth <dborth@gmail.com>
2026-09-03 14:31:12 -0600

    update comment

--- source/drivers/InputDriver.h
+++ source/drivers/InputDriver.h
@@ -23,7 +23,7 @@ class InputDriver
 		virtual void init() = 0;
 		virtual void shutdown() = 0;
 		
-		//! Polls the hardware and dispatches GuiInputPadData payloads to the UI
+		//! Polls the hardware and dispatches GuiInputPadData payloads
 		virtual void update() = 0;
 		
 		//! Requests a rumble event on the specified controller channel
