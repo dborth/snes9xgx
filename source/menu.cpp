46e7312749b56c41e559f14bd9931be6737178da
Daryl Borth <dborth@gmail.com>
2026-09-23 17:50:09 -0600

    fix triggers

--- source/menu.cpp
+++ source/menu.cpp
@@ -1446,7 +1446,7 @@ static void ControllerWindow()
 	trigLeft.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_LEFT);
 
 	GuiTrigger trigRight;
-	trigLeft.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_RIGHT);
+	trigRight.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_RIGHT);
 
 	GuiImageData arrowLeft(button_arrow_left_png);
 	GuiImage arrowLeftImg(&arrowLeft);
@@ -1526,7 +1526,7 @@ static void PlayerMappingWindow(int chan)
 	trigLeft.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_LEFT);
 
 	GuiTrigger trigRight;
-	trigLeft.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_RIGHT);
+	trigRight.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_RIGHT);
 
 	GuiImageData arrowLeft(button_arrow_left_png);
 	GuiImage arrowLeftImg(&arrowLeft);
@@ -3420,7 +3420,7 @@ static void ScreenZoomWindow()
 	trigLeft.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_LEFT);
 
 	GuiTrigger trigRight;
-	trigLeft.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_RIGHT);
+	trigRight.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_RIGHT);
 
 	GuiTrigger trigUp;
 	trigUp.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_UP);
@@ -3557,7 +3557,7 @@ static void ScreenPositionWindow()
 	trigLeft.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_LEFT);
 
 	GuiTrigger trigRight;
-	trigLeft.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_RIGHT);
+	trigRight.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_RIGHT);
 
 	GuiTrigger trigUp;
 	trigUp.setButtonOnlyInFocusTrigger(-1, INPUT_BTN_UP);
